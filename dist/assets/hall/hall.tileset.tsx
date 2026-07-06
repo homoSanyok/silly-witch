f5fa25b34ca4c1f2618b26794a6935fe0f49425f
homoSanyok <sadha.generalow@yandex.ru>
2026-07-06 14:47:35 +0300

    fix build path in action

--- dist/assets/hall/hall.tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="hall.tileset" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="hall.tileset.png" width="960" height="960"/>
-</tileset>
